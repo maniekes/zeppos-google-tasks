ef7307844cc5d700008c57fa77d523b50fa2c72e
rafik <rafal@klimonda.pl>
2023-02-10 23:19:08 +0100

    better task list

--- page/t-rex2/home/lists.page.js
+++ page/t-rex2/home/lists.page.js
@@ -7,7 +7,9 @@ const logger = DeviceRuntimeCore.HmLogger.getLogger('zeppos-google-tasks')
 
 Page({
     state: {
+        header: {title: 'Lists'},
         items: [{title: 'loading'}],
+        footer: {title: 'fajnie, nie?'},
         list: null,
         title: null,
         savedLists: {}
@@ -24,8 +26,8 @@ Page({
 
     build() {
         logger.debug('page build invoked')
-        this.state.title = this.initTitle()
         this.state.list = this.initList()
+        this.updateList()
         this.fetchLists()
     },
 
@@ -35,7 +37,15 @@ Page({
 
     updateList() {
         this.state.list.setProperty(hmUI.prop.UPDATE_DATA, {
-            data_array: this.state.items, data_count: this.state.items.length, on_page: 1
+            data_array: [this.state.header, ...this.state.items, this.state.footer],
+            data_count: this.state.items.length + 2,
+            on_page: 1,
+            data_type_config: [
+                {start: 0, end: 1, type_id: 1},
+                {start: 1, end: this.state.items.length + 1, type_id: 2},
+                {start: this.state.items.length + 1, end: this.state.items.length + 1, type_id: 3}
+            ],
+            data_type_config_count: 3
         })
     },
 
@@ -60,7 +70,7 @@ Page({
 
     scrollListItemClick(tthis, list, index) {
         logger.info('item clickedg')
-        const item = tthis.state.items[index]
+        const item = tthis.state.items[index - 1]
         const listObject = {id: item.id, title: item.title};
         tthis.state.savedLists.defaultList = listObject
         logger.info(`clicked ${item.title} / ${item.id}`)
@@ -74,25 +84,49 @@ Page({
     initList() {
         return hmUI.createWidget(hmUI.widget.SCROLL_LIST, {
             x: 0,
-            y: 60,
+            y: 0,
             h: px(454),
             w: px(454),
             item_space: 20,
-            item_config: [{
-                type_id: 1,
-                item_bg_color: 0x333333,
-                item_bg_radius: 20,
-                text_view: [{x: 0, y: 15, w: 454, h: 40, key: 'title', color: 0xffffff, text_size: 30}
-                    // ,{x: 0, y: 40, w: 200, h: 20, key: 'etag', color: 0xffffff}
-                ],
-                text_view_count: 1,
-                item_height: 80
-            }],
-            item_config_count: 1,
-            data_array: this.state.items,
-            data_count: this.state.items.length,
+            item_config: [
+                {
+                    type_id: 1,
+                    item_bg_color: 0x000000,
+                    item_bg_radius: 20,
+                    text_view: [{x: 0, y: 15, w: 454, h: 40, key: 'title', color: 0xffffff, text_size: 30}
+                    ],
+                    text_view_count: 1,
+                    item_height: 80
+                },
+                {
+                    type_id: 2,
+                    item_bg_color: 0x333333,
+                    item_bg_radius: 20,
+                    text_view: [{x: 0, y: 15, w: 454, h: 40, key: 'title', color: 0xffffff, text_size: 30}
+                        // ,{x: 0, y: 40, w: 200, h: 20, key: 'etag', color: 0xffffff}
+                    ],
+                    text_view_count: 1,
+                    item_height: 80
+                },
+                {
+                    type_id: 3,
+                    item_bg_color: 0x000000,
+                    item_bg_radius: 20,
+                    text_view: [{x: 0, y: 15, w: 454, h: 40, key: 'title', color: 0xffffff, text_size: 20}
+                    ],
+                    text_view_count: 1,
+                    item_height: 160
+                }],
+            item_config_count: 3,
+            data_array: [this.state.header, ...this.state.items, this.state.footer],
+            data_count: this.state.items.length + 2,
             item_click_func: (l, i) => this.scrollListItemClick(this, l, i),
-            data_type_config_count: 2
+            data_type_config: [
+                {start: 0, end: 1, type_id: 1},
+                {start: 1, end: this.state.items.length + 1, type_id: 2},
+                {start: this.state.items.length + 1, end: this.state.items.length + 1, type_id: 3}
+            ],
+            data_type_config_count: 3
         })
     },
 
